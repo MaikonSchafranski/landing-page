9efdcb7af5172eb8b200f467a62ec4ab58c0250b
MaikonSchafraski <maikonschfranski12@gmail.com>
2024-03-12 16:27:03 -0300

    feat: add Button Component

--- src/app/components/button-primary/button-primary.component.ts
+++ src/app/components/button-primary/button-primary.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+type BtnVariants = 'primary' | 'secondary';
 @Component({
   selector: 'app-button-primary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button-primary.component.html',
   styleUrl: './button-primary.component.scss',
 })
@@ -11,7 +13,7 @@ export class ButtonPrimaryComponent {
   @Input('btn-text') btnText: string = '';
   @Input() disabled = false;
   @Input() loading = false;
-  @Input() variant = BtnVariants = ;
+  @Input() variant: BtnVariants = 'primary';
   @Output('submit') onSubmit = new EventEmitter();
 
   submit() {
